710132f433e893cb8c35c235db1602e36035873d
Md Mridul <mdmridul608@hotmail.com>
2025-02-21 13:58:26 +0600

    Implement sing in screen layout and add scroll view

--- lib/ui/screens/sing_in_screen.dart
+++ lib/ui/screens/sing_in_screen.dart
@@ -13,30 +13,32 @@ class _SingInScreenState extends State<SingInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackgroundWidget(
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 60),
-                Text('Sing In', style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(hintText: 'Email'),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: 8),
-                TextFormField(
-                  decoration: InputDecoration(hintText: 'Password'),
-                  keyboardType: TextInputType.visiblePassword,
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.arrow_forward_ios_sharp),
-                ),
-              ],
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 60),
+                  Text('Sing In', style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'Password'),
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.arrow_forward_ios_sharp),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
