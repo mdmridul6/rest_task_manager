5b3d93c1b5efef1a877894ef5f0b2ad863388aaf
Md Mridul <mdmridul608@hotmail.com>
2025-02-25 13:10:48 +0600

    Update NDK version and add user profile header
    - Updated the NDK version in `android/app/build.gradle.kts` to `27.0.12077973`.
    - Added a user profile header in `all_task_screen.dart` that includes a profile picture, name, email and logout button.
    - Added `primaryColor` in app color.

--- lib/ui/screens/tasks/all_task_screen.dart
+++ lib/ui/screens/tasks/all_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_task_manager/ui/utility/app_color.dart';
 import 'package:rest_task_manager/ui/widgets/network_cashed_image.dart';
 
 class AllTaskScreen extends StatefulWidget {
@@ -13,10 +14,39 @@ class _AllTaskScreenState extends State<AllTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColor.primaryColor,
         leading: Padding(
           padding: const EdgeInsets.all(8),
-          child: NetworkCashedImage(url: ''),
+          child: CircleAvatar(
+            child: NetworkCashedImage(
+              url:
+                  'https://www.torrentbd.net/posters/C1dcoocS8XLX4WZpxZRfDwYY32176689.jpg',
+            ),
+          ),
         ),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Jone doye',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+            Text(
+              'jone@gmail.com',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.logout, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
